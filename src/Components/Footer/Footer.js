f0621a8528743f1c409dec8368aef61f41eb62d7
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-04 13:20:23 +0600

    update footer section and not found component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 
 const Footer = () => {
-  const { key } = useLocation();
   return (
-    <footer className={key.includes("default") ? "d-none" : "null"}>
-      <div className="text-center mt-5 bg-dark text-white py-5">
-        <h5>
-          &copy; copyright <span className="brand">A</span>rchiTech
-        </h5>
-        <h6> All rights reserved 2020-2022</h6>
-        <p>Development by &hearts; Mohammad Shariful</p>
-      </div>
+    <footer className="text-center mt-5 bg-dark text-white py-5">
+      <h5>
+        &copy; copyright <span className="brand">A</span>rchiTech
+      </h5>
+      <h6> All rights reserved 2020-2022</h6>
+      <p>Development by &hearts; Mohammad Shariful</p>
     </footer>
   );
 };
